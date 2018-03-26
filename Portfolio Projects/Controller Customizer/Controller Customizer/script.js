38d3aa27b4345919e8e3a6289ed346f0a3361438
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-03-26 17:30:07 -0400

    footer highlight fixed, background change on click

--- Portfolio Projects/Controller Customizer/Controller Customizer/script.js
+++ Portfolio Projects/Controller Customizer/Controller Customizer/script.js
@@ -2,17 +2,13 @@
 const logos = document.querySelectorAll('.logo-caption');
 // console.log(logos);
 // console.log(logos[0]);
+
 const highlight = document.createElement('span');
-const footer = document.querySelector('.footer');
 
 highlight.classList.add('highlight');
-
-// document.body.append(highlight);
-footer.append(highlight);
-
+document.body.append(highlight);
 
 function highlightLink() {
-  const footerCoords = footer.getBoundingClientRect();
   const linkCoords = this.getBoundingClientRect();
 
   highlight.style.width = `${linkCoords.width}px`;
@@ -20,21 +16,8 @@ function highlightLink() {
 
   // compensate for if window has been scrolled
   highlight.style.left = `${linkCoords.left + window.scrollX}px`;
-  highlight.style.top = `${linkCoords.top - footerCords.top + window.scrollY}px`;
+     highlight.style.top = `${linkCoords.top + window.scrollY}px`;
 }
-
-
-const batman = document.getElementById('batman-logo');
-console.log(batman);
-const sticker = document.getElementById('sticker');
-
-batman.addEventListener("mousedown", event => {
-  sticker.src = "media/images/sticker-batman-blue.png";
-  document.body.style.backgroundImage = "url('media/images/dark-transparent-background.png'), url('media/images/batman-background.jpg')";
-});
-
-
-
 
 
 // when mouse moves over each element in "logos"
